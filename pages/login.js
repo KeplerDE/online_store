b638b745be1dff275d07c93d84f010e4ab1111ee
keple <keplerde@gmail.com>
2023-12-08 07:08:36 +0100

    next auth-signIn

--- pages/login.js
+++ pages/login.js
@@ -1,52 +1,49 @@
-  import React, { useState } from 'react';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/router';
+import { signIn } from 'next-auth/react'
 
-export default function RegisterPage() {
-  const [name, setName] = useState('Test');
-  const [email, setEmail] = useState('test@gmail.com');
-  const [password, setPassword] = useState('test');
+
+
+
+export default function Login() {
+  const [email, setEmail] = useState(''); 
+  const [password, setPassword] = useState(''); 
   const [loading, setLoading] = useState(false);
+
   const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+
     try {
-      const response = await fetch(`${process.env.API}/register`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, email, password }),
+      // Perform the sign in using next-auth
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password
       });
-      const data = await response.json();
-      if (!response.ok) {
-        toast.error(data.err);
+
+      if (result?.error) {
+        toast.error(result?.error);
+        setLoading(false);
       } else {
-        toast.success(data.success);
-        router.push('/login');
+        toast.success('Successfully logged in!');
+        router.push('/');
       }
-    } catch (err) {
-      console.error(err);
-      toast.error('An error occurred during registration.');
-    } finally {
+    } catch (error) {
+      toast.error('Failed to log in. Please try again.');
       setLoading(false);
     }
   };
-
   return (
     <main>
       <div className="container">
         <div className="row d-flex justify-content-center align-items-center vh-100">
           <div className="col-lg-5 bg-light p-5 shadow">
-            <h2 className="mb-4">Register</h2>
+            <h2 className="mb-4">Login</h2>
             <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="form-control mb-4"
-                placeholder="Your name"
-              />
               <input
                 type="email"
                 value={email}
@@ -63,7 +60,7 @@ export default function RegisterPage() {
               />
               <button
                 className="btn btn-primary btn-raised"
-                disabled={loading || !name || !email || !password}
+                disabled={loading || !email || !password}
               >
                 {loading ? "Please wait.." : "Submit"}
               </button>
