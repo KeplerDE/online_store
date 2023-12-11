9fe3bdfcee33eb7993360213803fb6c0268b5ca4
keple <keplerde@gmail.com>
2023-12-11 07:30:10 +0100

    next_auth configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-// next.config.js
 const config = require("./config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- env: {
- DB_URI: config.DB_URI,
- API: config.API,
- },
+  env: {
+    DB_URI: config.DB_URI,
+    API: config.API,
+    NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
+  },
 };
+
 module.exports = nextConfig;
---
+++ utils/authOptions.js
@@ -0,0 +1,45 @@
+import CredentialsProvider from "next-auth/providers/credentials"; 
+import User from "@/models/user";
+import bcrypt from "bcrypt"; 
+import dbConnect from "@/utils/dbConnect"; 
+
+export const authOptions = {
+  session: {
+    strategy: "jwt", // Используем JWT для сессии
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        await dbConnect(); // Подключаемся к базе данных
+
+        const { email, password } = credentials; // Получаем email и пароль из переданных учетных данных
+        
+        // Поиск пользователя по email
+        const user = await User.findOne({ email: email.toLowerCase() });
+        if (!user) {
+          throw new Error("Invalid email or password"); // Если пользователь не найден, бросаем ошибку
+        }
+
+        // Проверяем, есть ли у пользователя пароль (проверка на случай, если регистрация была через соцсеть)
+        if (!user.password) {
+          throw new Error("Пожалуйста, войдите тем способом, который вы использовали при регистрации");
+        }
+
+        // Сверяем предоставленный пароль с хэшированным паролем из базы данных
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatched) {
+          throw new Error("Invalid email or password"); // Если пароли не совпадают, бросаем ошибку
+        }
+
+        // Возвращаем объект пользователя без пароля
+        const { password: _, ...userWithoutPassword } = user.toObject();
+        return userWithoutPassword;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET, // Секретный ключ для подписи сессии, должен быть задан в переменных окружения
+  pages: {
+    signIn: "/auth/signin", // URL страницы для входа
+  },
+
+};
