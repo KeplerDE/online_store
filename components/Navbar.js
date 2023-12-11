014b74c4172dd476cc4635fc9549cee5c33f84dd
keple <keplerde@gmail.com>
2023-12-11 20:55:02 +0100

    session_provider

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,14 @@
 // components/Navbar.js
 import Link from 'next/link';
+import { useSession, signOut } from "next-auth/react";
+
+
+
+
 
 const Navbar = () => {
+  const { data, status, loading } = useSession();
+  console.log(data, status);
   return (
     <nav className="nav shadow p-2 justify-content-between mb-3">
       <Link className="navbar-brand" href="/">
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-// pages/_app.js
 import '../styles/globals.css';
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
+import { SessionProvider } from 'next-auth/react'; 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
