95a5304579aa1da87845a47d10ff552705d32ca1
keple <keplerde@gmail.com>
2023-12-12 07:30:09 +0100

    login and logout information

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,17 +13,29 @@ const Navbar = () => {
     <nav className="nav shadow p-2 justify-content-between mb-3">
       <Link className="navbar-brand" href="/">
         My Ecommerce Site 🎄
-      </Link>
-
-
-      <div className='d-flex'>
-        <Link href="/login" className='nav-link'>
+        </Link>
+    {status === "authenticated" ? (
+      <div className="d-flex">
+        <Link className="nav-link" href="/dashboard/user">
+          {data?.user?.name}
+        </Link>
+        <a
+          className="nav-link pointer"
+          onClick={() => signOut({ callbackUrl: "/login" })}
+        >
+          Logout
+        </a>
+      </div>
+    ) : (
+      <div className="d-flex">
+        <Link className="nav-link" href="/login">
           Login
         </Link>
-        <Link href="/register" className='nav-link'>
+        <Link className="nav-link" href="/register">
           Register
         </Link>
-      </div>
+      </div>)}
+
 
     </nav>
   );
