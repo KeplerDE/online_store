dc7bcdaf87f559778860cc743b67cea5985d3809
keple <keplerde@gmail.com>
2023-12-13 15:34:57 +0100

    protect_middleware

---
+++ pages/dashboard/user/index.js
@@ -0,0 +1,14 @@
+export default function Userdashboard() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="column">
+                    <p className="lead">
+                        Dashboard
+                    </p>
+                    <hr />...
+                </div>
+            </div>
+        </div>
+    )
+}
