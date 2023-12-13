f82acfde6f3a837ed14cfb050e625aedc800c19d
keple <keplerde@gmail.com>
2023-12-13 15:20:44 +0100

    loading_page_2

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
-import { SessionProvider } from 'next-auth/react'; 
+import { SessionProvider } from 'next-auth/react';
+import Loading from '../components/Loading'; 
+import RootLayout from '../components/RootLayout'; // Импортируем RootLayout
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    // Подписываемся на события маршрутизации
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    // Отписываемся от событий при размонтировании компонента
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      {loading && <Loading />} {/* Отображаем Loading, если loading === true */}
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
     </SessionProvider>
   );
 }
