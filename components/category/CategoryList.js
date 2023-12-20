a71e2985f861f3d392b75965abacf877950ca71f
keple <keplerde@gmail.com>
2023-12-20 08:15:12 +0100

    categoryList

--- components/category/CategoryList.js
+++ components/category/CategoryList.js
@@ -1,11 +1,30 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import { useCategory } from "@/context/category";
 
-function CategoryList() {
+export default function CategoryList() {
+  // Using category context
+  const { categories, fetchCategories, setUpdatingCategory } = useCategory();
+
+  // Fetch categories on component mount
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
-    <div>
-      
+    <div className="container mb-5">
+      <div className="row">
+        <div className="col">
+          {categories.map((c) => (
+            <button
+              className="btn"
+              onClick={() => setUpdatingCategory(c)}
+            >
+              {c.name}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default CategoryList
