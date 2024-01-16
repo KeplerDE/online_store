275091d3682e5b4bcbcb8fb3325054b37a755cc8
keple <keplerde@gmail.com>
2024-01-16 10:18:17 +0100

    products_list with pagination

---
+++ app/api/product/route.js
@@ -0,0 +1,39 @@
+// api/product/route
+import { NextResponse } from 'next/server';
+import dbConnect from '@/utils/dbConnect';
+import Product from '@/models/product';
+import queryString from 'query-string';
+
+export async function GET(req) {
+  try {
+    // Подключаемся к базе данных
+    await dbConnect();
+
+    // Разбор параметров запроса из URL
+    const searchParams = queryString.parseUrl(req.url).query;
+    const { page } = searchParams || {};
+    const pageSize = 6; // Количество продуктов на странице
+
+    // Определение текущей страницы и сдвига
+    const currentPage = Number(page) || 1;
+    const skip = (currentPage - 1) * pageSize;
+
+    // Получение общего количества продуктов и данных продуктов для текущей страницы
+    const totalProducts = await Product.countDocuments({});
+    const products = await Product.find({})
+      .skip(skip)
+      .limit(pageSize)
+      .sort({ createdAt: '-1' });
+
+    // Возвращаем продукты и информацию о странице
+    return NextResponse.json({
+      products,
+      currentPage,
+      totalPages: Math.ceil(totalProducts / pageSize),
+    }, { status: 200 });
+  } catch (err) {
+    // В случае ошибки выводим ее в консоль и возвращаем ответ с ошибкой
+    console.log(err);
+    return NextResponse.json({ err: err.message }, { status: 500 });
+  }
+}
