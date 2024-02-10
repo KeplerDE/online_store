595a32abc914dd846b378c3c3c762be2ed4065fc
keple <keplerde@gmail.com>
2024-02-10 02:33:34 +0100

    modified ProductComponent

--- components/product/ProductFilter.js
+++ components/product/ProductFilter.js
@@ -1,24 +1,26 @@
 "use client";
-
-import { priceRanges } from "../../utils/filterData"
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useCategory } from "@/context/category";
-import Stars from "../../components/product/Stars";
-import { useTag } from "@/context/tag";
-import { useProduct } from "@/context/product";
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router'; 
+import { useCategory } from '@/context/category';
+import { useTag } from '@/context/tag';
+import { useProduct } from '@/context/product';
+import Stars from '../../components/product/Stars';
+import { priceRanges } from '../../utils/filterData';
 
 export default function ProductFilter({ searchParams }) {
-  const pathname = "/shop";
+  const pathname = '/shop';
   const { minPrice, maxPrice, ratings, category, tag, brand } = searchParams;
 
-
-  // context
+  // Получение контекстов
   const { fetchCategoriesPublic, categories } = useCategory();
   const { fetchTagsPublic, tags } = useTag();
   const { fetchBrands, brands } = useProduct();
+  const router = useRouter();
 
+  // 
+  const activeButton = 'btn btn-primary btn-raised mx-1 rounded-pill';
+  const button = 'btn btn-raised mx-1 rounded-pill';
 
   useEffect(() => {
     fetchCategoriesPublic();
@@ -26,67 +28,40 @@ export default function ProductFilter({ searchParams }) {
     fetchBrands();
   }, []);
 
-  const router = useRouter();
-
-  const activeButton = "btn btn-primary btn-raised mx-1 rounded-pill";
-  const button = "btn  btn-raised mx-1 rounded-pill";
-
   const handleRemoveFilter = (filterName) => {
     const updatedSearchParams = { ...searchParams };
-   
 
-    // if filterName is string
-    if (typeof filterName === "string") {
+    if (typeof filterName === 'string') {
       delete updatedSearchParams[filterName];
+    } else if (Array.isArray(filterName)) {
+      filterName.forEach((name) => delete updatedSearchParams[name]);
     }
 
-    // if filterName is array
-    if (Array.isArray(filterName)) {
-      filterName?.forEach((name) => {
-        delete updatedSearchParams[name];
-      });
-    }
-
-    // reset page to 1 when applying new filtering options
     updatedSearchParams.page = 1;
-
-    const queryString = new URLSearchParams(updatedSearchParams).toString();
-    const newUrl = `${pathname}?${queryString}`;
-    router.push(newUrl);
+    router.push({ pathname, query: updatedSearchParams });
   };
 
   return (
     <div>
       <p className="lead">Filter Products</p>
-      <Link className="text-danger" href="/shop">
+      <Link href="/shop" className="text-danger">
         Clear Filters
       </Link>
+
+      {/* Price Filter */}
       <p className="mt-4 alert alert-primary">Price</p>
       <div className="row d-flex align-items-center mx-1">
         {priceRanges.map((range) => {
-          const url = {
-            pathname,
-            query: {
-              ...searchParams,
-              minPrice: range?.min,
-              maxPrice: range?.max,
-              page: 1,
-            },
-          };
-
-          const isActive =
-            minPrice === String(range?.min) && maxPrice === String(range?.max);
+          const isActive = minPrice === String(range.min) && maxPrice === String(range.max);
+          const url = { pathname, query: { ...searchParams, minPrice: range.min, maxPrice: range.max, page: 1 } };
 
           return (
-            <div key={range?.label}>
+            <div key={range.label}>
               <Link href={url} className={isActive ? activeButton : button}>
-                {range?.label}
+                {range.label}
               </Link>
               {isActive && (
-                <span
-                  onClick={() => handleRemoveFilter(["minPrice", "maxPrice"])}
-                  className="pointer"
-                >
+                <span onClick={() => handleRemoveFilter(['minPrice', 'maxPrice'])} className="pointer">
                   X
                 </span>
               )}
@@ -95,32 +70,20 @@ export default function ProductFilter({ searchParams }) {
         })}
       </div>
 
+      {/* Ratings Filter */}
       <p className="mt-4 alert alert-primary">Ratings</p>
       <div className="row d-flex align-items-center mx-1">
-        {[5, 4, 3, 2, 1].map((ratingValue) => {
-          const isActive = String(ratings) === String(ratingValue);
-          const url = {
-            pathname,
-            query: {
-              ...searchParams,
-              ratings: ratingValue,
-              page: 1,
-            },
-          };
+        {[5, 4, 3, 2, 1].map((rating) => {
+          const isActive = ratings === String(rating);
+          const url = { pathname, query: { ...searchParams, ratings: rating, page: 1 } };
 
           return (
-            <div key={ratingValue}>
-              <Link
-                href={url}
-                className={isActive ? "btn btn-primary btn-raised mx-1 rounded-pill" : "btn btn-raised mx-1 rounded-pill"}
-              >
-                <Stars rating={ratingValue} />
+            <div key={rating}>
+              <Link href={url} className={isActive ? activeButton : button}>
+                <Stars rating={rating} />
               </Link>
               {isActive && (
-                <span
-                  onClick={() => handleRemoveFilter("ratings")}
-                  className="pointer"
-                >
+                <span onClick={() => handleRemoveFilter('ratings')} className="pointer">
                   X
                 </span>
               )}
@@ -129,28 +92,20 @@ export default function ProductFilter({ searchParams }) {
         })}
       </div>
 
+      {/* Categories Filter */}
       <p className="mt-4 alert alert-primary">Categories</p>
       <div className="row d-flex align-items-center mx-1 filter-scroll">
-        {categories?.map((c) => {
-          const isActive = categories === c._id;
-          const url = {
-            pathname,
-            query: {
-              ...searchParams,
-              category: c?._id,
-              page: 1,
-            },
-          };
+        {categories.map((c) => {
+          const isActive = category === c._id;
+          const url = { pathname, query: { ...searchParams, category: c._id, page: 1 } };
+
           return (
             <div key={c._id}>
               <Link href={url} className={isActive ? activeButton : button}>
-                {c?.name}
+                {c.name}
               </Link>
               {isActive && (
-                <span
-                  onClick={() => handleRemoveFilter("category")}
-                  className="pointer"
-                >
+                <span onClick={() => handleRemoveFilter('category')} className="pointer">
                   X
                 </span>
               )}
@@ -159,73 +114,55 @@ export default function ProductFilter({ searchParams }) {
         })}
       </div>
 
-      
+      {/* Tags Filter */}
       {category && (
-      <>
-        <p className="mt-4 alert alert-primary">Tags</p>
-        <div className="row d-flex align-items-center mx-1 filter-scroll">
-          {tags
-            ?.filter((t) => t?.parentCategory?._id === category)
-            ?.map((t) => {
-              const isActive = tag === t._id;
-              const url = {
-                pathname,
-                query: {
-                  ...searchParams,
-                  tag: t._id,
-                  page: 1,
-                },
-              };
-              return (
-                <div key={t._id}>
-                  <Link href={url} className={isActive ? 'activeButton' : 'button'}>
-                    {t.name}
-                  </Link>
-                  {isActive && (
-                    <span onClick={() => handleRemoveFilter("tag")} className="pointer">
-                      X
-                    </span>
-                  )}
-                </div>
-              );
-            })}
-        </div>
-      </>
-    )}
+        <>
+          <p className="mt-4 alert alert-primary">Tags</p>
+          <div className="row d-flex align-items-center mx-1 filter-scroll">
+            {tags
+              .filter((t) => t.parentCategory && t.parentCategory._id === category) 
+              .map((t) => {
+                const isActive = tag === t._id; 
+                const url = { pathname, query: { ...searchParams, tag: t._id, page: 1 } }; 
 
-    <p className="mt-4 alert alert-primary">Brands</p>
-    <div className="row d-flex align-items-center mx-1 filter-scroll">
-      {brands?.map((b) => {
-        const isActive = brand === b;
-        const url = {
-          pathname,
-          query: {
-            ...searchParams,
-            brand: b,
-            page: 1,
-          },
-        };
-        return (
-          <div key={b}>
-            <Link href={url} className={isActive ? activeButton : button}>
-              {b}
-            </Link>
-            {isActive && (
-              <span
-                onClick={() => handleRemoveFilter("brand")}
-                className="pointer"
-              >
-                X
-              </span>
-            )}
+                return (
+                  <div key={t._id}>
+                    <Link href={url} className={isActive ? activeButton : button}>
+                      {t.name}
+                    </Link>
+                    {isActive && (
+                      <span onClick={() => handleRemoveFilter('tag')} className="pointer">
+                        X
+                      </span>
+                    )}
+                  </div>
+                );
+              })}
           </div>
-        );
-      })}
-    </div>
-        
+        </>
+      )}
       {/* <pre>{JSON.stringify(tags, null, 4)}</pre> */}
+      {/* Brands Filter */}
+      <p className="mt-4 alert alert-primary">Brands</p>
+      <div className="row d-flex align-items-center mx-1 filter-scroll">
+        {brands.map((b) => {
+          const isActive = brand === b;
+          const url = { pathname, query: { ...searchParams, brand: b, page: 1 } };
 
-
+          return (
+            <div key={b}>
+              <Link href={url} className={isActive ? activeButton : button}>
+                {b}
+              </Link>
+              {isActive && (
+                <span onClick={() => handleRemoveFilter('brand')} className="pointer">
+                  X
+                </span>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
