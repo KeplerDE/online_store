09fd308661074f7d59a2332abf5a2998a5694849
keple <keplerde@gmail.com>
2024-02-20 15:28:49 +0100

    Rating_distribution

--- components/product/UserReviews.js
+++ components/product/UserReviews.js
@@ -1,16 +1,20 @@
+import RatingDistribution from "@/components/product/RatingDistribution";
 import Stars from "@/components/product/Stars";
 
 export default function UserReviews({ reviews }) {
-  return (
-    <>
-      {reviews && reviews.length > 0 ? (
+  // Render either the list of reviews or a message indicating there are no reviews
+  const renderReviews = () => {
+    if (reviews && reviews.length > 0) {
+      return (
         <div>
-          {/* User reviews list */}
+          <RatingDistribution reviews={reviews} />
           <ul className="list-group mt-4 bg-white">
-            {reviews.map(review => (
-              <li className="list-group-item mb-1" key={review._id}>
+            {reviews.map((review) => (
+              <li key={review._id} className="list-group-item mb-1">
                 <div>
-                  <p><strong>{review.postedBy.name}</strong></p>
+                  <p>
+                    <strong>{review.postedBy.name}</strong>
+                  </p>
                   <Stars rating={review.rating} />
                   {review.comment && <p className="mt-3">{review.comment}</p>}
                 </div>
@@ -18,9 +22,15 @@ export default function UserReviews({ reviews }) {
             ))}
           </ul>
         </div>
-      ) : (
-        <p>No reviews yet.</p>
-      )}
+      );
+    } else {
+      return <p>No reviews yet.</p>;
+    }
+  };
+
+  return (
+    <>
+      {renderReviews()}
     </>
   );
 }
