f407a4858b2af482a0666adc9d405157edb43de3
keple <keplerde@gmail.com>
2024-02-21 12:18:02 +0100

    cart_page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,18 @@
-// components/Navbar.js
+import { BsFillCartCheckFill } from 'react-icons/bs';
 import Link from 'next/link';
 import { useSession, signOut } from "next-auth/react";
 import { useProduct } from '@/context/product';
-
+import { useCart } from '@/context/cart'; 
 
 const Navbar = () => {
   const { data, status } = useSession();
-
   const {
     productSearchQuery,
     setProductSearchQuery,
     fetchProductSearchResults,
   } = useProduct();
-  
+  const { cartItems } = useCart(); 
+
   return (
     <nav className="nav shadow p-2 justify-content-between mb-3">
       <div className="d-flex flex-grow-1">
@@ -24,6 +24,7 @@ const Navbar = () => {
         </Link>
       </div>
 
+      {/* Форма поиска */}
       <form className="d-flex mx-2" 
       role="search" 
       onSubmit={fetchProductSearchResults}>
@@ -40,20 +41,36 @@ const Navbar = () => {
         </button>
       </form>
 
-      {status === "authenticated" ? (
-        <div className="d-flex">
-          <Link href={`/dashboard/${data?.user?.role === "admin" ? "admin" : "user"}`}>
-            <span className="nav-link">{data?.user?.name} {data?.user?.role}</span>
+      {status === "authenticated" && ( // Показываем корзину только для аутентифицированных пользователей
+        <>
+          {/* Ссылка на корзину с иконкой и количеством товаров */}
+          <Link href="/cart" className="nav-link text-danger">
+            <BsFillCartCheckFill size={25} />
+            {cartItems?.length > 0 && (
+              <span className="cart-item-count">
+                {cartItems.length}
+              </span>
+            )}
           </Link>
-          <span className="nav-link pointer" onClick={() => signOut({ callbackUrl: "/login" })}>
-            Logout
-          </span>
-        </div>
-      ) : status === "loading" ? (
+
+          <div className="d-flex">
+            <Link href={`/dashboard/${data?.user?.role === "admin" ? "admin" : "user"}`}>
+              <span className="nav-link">{data?.user?.name} ({data?.user?.role})</span>
+            </Link>
+            <span className="nav-link pointer" onClick={() => signOut({ callbackUrl: "/login" })}>
+              Logout
+            </span>
+          </div>
+        </>
+      )}
+
+      {status === "loading" && (
         <div className="d-flex">
-          <span className="nav-link text-danger">Loading</span>
+          <span className="nav-link text-danger">Loading...</span>
         </div>
-      ) : (
+      )}
+
+      {status !== "authenticated" && status !== "loading" && (
         <div className="d-flex">
           <Link href="/login">
             <span className="nav-link">Login</span>
